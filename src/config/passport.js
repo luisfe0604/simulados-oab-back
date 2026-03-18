46d0f2e4b3323fd5213babbafec7cec2b4b5857d
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-03-18 16:11:31 -0300

    update back database

--- src/config/passport.js
+++ src/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/users/auth/google/callback",
+      callbackURL: "https://simulados-oab-back.onrender.com/users/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
