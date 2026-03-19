8e5681a3995df950d83d0dcac8faed14a5ccb2d2
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-03-19 17:52:44 -0300

    update back billing

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken");
 function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
 
+  console.log(authHeader)
+
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ error: "Token não fornecido" });
   }
