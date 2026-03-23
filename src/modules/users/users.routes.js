7ce5db5efaff5dfc4f57146338bb2e06f9ec98c0
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-03-23 15:04:26 -0300

    update back database

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -2,30 +2,12 @@ const express = require("express");
 const controller = require("./users.controller");
 const passport = require("passport");
 const authMiddleware = require("../../middlewares/auth.middleware");
-const Stripe = require("stripe");
-const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
 router.get("/me", authMiddleware, controller.me);
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-function hasAccess(user) {
-  if (user.subscription_status === "active") return true;
-
-  if (
-    user.subscription_status === "trial" &&
-    user.trial_end &&
-    new Date(user.trial_end).getTime() > Date.now()
-  ) {
-    return true;
-  }
-
-  return false;
-}
 
 router.get(
   "/auth/google",
