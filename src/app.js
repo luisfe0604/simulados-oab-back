ea9e08c4211cc6d09922f79e1932b7392eb7b042
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-03-23 11:06:59 -0300

    update back database

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 const billing = require("./modules/billing/billing.routes");
+const billingController = require("./modules/billing/billing.controller");
 const passport = require("./config/passport");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(passport.initialize());
-app.use("/billing/webhook", express.raw({ type: "application/json" }));
-app.use("/billing", billing);
 
+app.post(
+  "/billing/webhook",
+  express.raw({ type: "application/json" }),
+  billingController.stripeWebhook,
+);
+
+app.use(express.json());
+
+app.use(passport.initialize());
+app.use("/billing", billing);
 app.use(routes);
 
 module.exports = app;
