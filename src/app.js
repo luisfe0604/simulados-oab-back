1b0f73659de70002cddf5dee93a3e65430c90c78
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-05-28 13:53:34 -0300

    mapa festa escola

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const routes = require("./routes");
 const billing = require("./modules/billing/billing.routes");
 const billingController = require("./modules/billing/billing.controller");
 const passport = require("./config/passport");
-const http = require('http'); //api teste
-const { initWebSocket } = require('./modules/useTestsGenerics/websocket/websocket');//api teste
 
 const app = express();
 
@@ -24,9 +22,5 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use("/billing", billing);
 app.use(routes);
-
-const server = http.createServer(app);//api teste
-
-initWebSocket(server);//api teste
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
+const http = require('http');//api teste
 const app = require("./app");
+
+const {
+  initWebSocket
+} = require('./modules/useTestsGenerics/websocket/websocket');//api teste
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = http.createServer(app);//api teste
+
+initWebSocket(server);//api teste
+
+server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
