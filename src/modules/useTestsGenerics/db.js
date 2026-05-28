a4fbc4292bcc74929fa5f48e3a1f819dfc45bcdc
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-05-28 13:20:36 -0300

    mapa festa escola

--- src/modules/useTestsGenerics/db.js
+++ src/modules/useTestsGenerics/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres.ngfuquqknytsohukhqvb:luis0604@aws-0-us-west-2.pooler.supabase.com:6543/postgres',
+  connectionString: 'postgresql://postgres.ngfuquqknytsohukhqvb:2A2DMGjnPCzCgcMo@aws-0-us-west-2.pooler.supabase.com:6543/postgres',
 });
 
 module.exports = pool;
