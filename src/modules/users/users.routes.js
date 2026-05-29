238a1db6ae34398abe8cbc51b2c4aa83d19b34d1
luisfe0604 <106082104+luisfe0604@users.noreply.github.com>
2026-05-29 12:41:29 -0300

    enem

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -14,7 +14,7 @@ router.get("/metrics", authMiddleware, adminMiddleware, controller.getMetrics);
 router.get("/auth/google", (req, res, next) => {
   passport.authenticate("google", {
     scope: ["profile", "email"],
-    state: req.query.state || "oab",
+    state: req.query.system || "oab",
   })(req, res, next);
 });
 
